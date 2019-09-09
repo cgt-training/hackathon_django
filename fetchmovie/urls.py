61030d3509177fd85ebab9edc191f927d355aa87
Vaibhav Sharma <vaibhav.sharma@cgt.co.in>
2019-09-09 12:41:59 +0530

    Merging with dev branch

--- fetchmovie/urls.py
+++ fetchmovie/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('recommended', views.index, name='index'),
     path('topmovies', views.top_movies, name='top'),
     path('moviedetail', views.singleMovieRequest, name='moviedetail'),
-    path('createdataset', views.getMoviesDetail, name='createdataset')
+    path('createdataset', views.getMoviesDetail, name='createdataset'),
+        # url(r'^objects/(?P<oid>[0-9]+)/$', views.object_specific_view, name='objects'),
+
 ]
--- fetchmovie/views.py
+++ fetchmovie/views.py
@@ -37,12 +37,18 @@ except CParserError as e:
 	print(e)
 	raise formbuilder_core.views.ValidationException('report', 'insufficient')
 
+user_genre_li = []
+
 def index(request):
 	
 	session_id = 0
+	if 'firstCall' not in request.session:
+		user_genre_li = getUserSpecificGenre(request, request.session['id'])
+		request.session['user_genre'] = user_genre_li
 	if 'id' in request.session:
 		session_id = request.session['id']
 
+	# print(user_genre_li)
 	ratingsMovies = ratings
 	ratingsMovies = pd.merge(movies,ratingsMovies).drop(['genres','timestamp'],axis=1)
 	# ratingsMovies_head = ratingsMovies.head(20)
@@ -94,16 +100,66 @@ def index(request):
 	movie_names_arrays = list(movie_names_keys)
 	#print(movie_names_arrays)
 
-	#print(type(sim_movies_out))
+	"""
+		this function will find details of the movie.
+	"""
+	movie_detail_array = []
+
+	movie_detail_array = getMovieDetail(movie_names_arrays)
+
+	# print('movie_detail_array[0][title]')
+	print(movie_detail_array[0].name)
+
 	my_context ={
-		"movie_list": movie_names_arrays,
-		"number_movie": 20
+		# "movie_list": movie_names_arrays,
+		"movie_list": movie_detail_array,
+		"number_movie": 20,
+		"user_genre_list": user_genre_li
 	}
 	#return HttpResponse("<a href='/movies/dummy'>Hello, world. index.</a>")
 	
 	return render(request,"fetchmovie/index.html",my_context)
 
+
+def getMovieDetail(movie_names_arrays):
+	links_small = links
+	moviesVar = movies
+	movies_details = moviedetail
+
+
+	moviesVar = pd.merge(moviesVar,links_small).drop(['imdbId'],axis=1)
+
+	moviesVar = moviesVar.fillna(0)
+
+	moviesVar['tmdbId'] = moviesVar['tmdbId'].astype('int')
+
+	moviesVar = moviesVar.rename(columns={"tmdbId": "id"})
+
+	links_small_filter = links_small[links_small['tmdbId'].notnull()]['tmdbId'].astype('int')
+
+	movies_details = movies_details[movies_details['id'].apply(lambda x : x.isnumeric())]
+	movies_details['id'] = movies_details['id'].astype('int')
+	movies_details = movies_details.drop(['original_title','title'],axis=1)
+
+	movies_details_filter = movies_details[movies_details['id'].isin(links_small_filter)]
+	movies_details_filter = moviesVar.merge(movies_details_filter, left_on='id', right_on='id')
+
+	# print(movies_details_filter.keys())
+
+	movies_details_filter.set_index('title', inplace=True)
+
+	# print(movies_details_filter.keys())
+	movie_det_array = []
+	# detail = movies_details_filter.loc['Indiana Jones and the Temple of Doom (1984)']
+	for movie in movie_names_arrays:
+		detail = movies_details_filter.loc[movie]
+		movie_det_array.append(detail)
+
+	return movie_det_array
+	
+
 def get_similar(movie_name,rating,corrMatrix):
+	# print("get_similar()")
 	try:
 		similar_ratings = corrMatrix[movie_name]*(rating-2.5)
 		similar_ratings = similar_ratings.sort_values(ascending=False)
@@ -112,24 +168,94 @@ def get_similar(movie_name,rating,corrMatrix):
 		return similar_ratings
 	except Exception as e:
 		# return ['continue', -50]
-		print(e)
+		# print(e)
+		print('')
+
+def getUserSpecificGenre(request, userId_P):
+	request.session['firstCall'] = 'called'
+	ratingsVar = ratings
+	moviesVar = movies
+
+	ratingsVar = pd.merge(moviesVar,ratingsVar).drop(['timestamp'],axis=1)
+
+	ratingsVar.head()
+
+	user_rating = pd.DataFrame()
+
+	total_users = 650
+
+	combined_genres_array = []
+
+	# user_rating = ratingsVar.query('userId == 1')
+	user_rating = ratingsVar.query('userId == %s' %(userId_P))
+
+	# making boolean series for a team name 
+	filter1 = user_rating["rating"]<5.01
+	filter2 = user_rating["rating"]>3.99
+
+	filtered_data = user_rating.where(filter1 & filter2, inplace = False).dropna()
+
+	genres_users = filtered_data['genres']
+
+	print('-----------***************-------------')
+	final_genres_array = []
+
+	for genre in genres_users:
+		value = genre.split('|')
+		for word in value:
+			if word not in final_genres_array:
+				final_genres_array.append(word)
+
+	genres_array = []
+
+	if len(final_genres_array) > 10:
+		for word in final_genres_array:
+			count = 0
+			genre_obj = {'genre':word,'count':0}
+			for genre in genres_users:
+				present = word in genre
+				if present:
+					count = count + 1
+			genre_obj['count']	= count	
+			genres_array.append(genre_obj)
+
+	sorted_genre_count = sorted(genres_array, key = lambda i: i['count'],reverse=True) 
+	print(sorted_genre_count)		
+
+	index = 0
+
+	print('-----------***************-------------')
+
+	final_genres_list = []
+
+	for obj in sorted_genre_count:
+		genre_value = obj['genre']
+		final_genres_list.append(genre_value)
+		index = index + 1
+		if index > 7:
+			break
+
+	# print(final_genres_list)
+	return final_genres_list
+
 
 def top_movies(request):
 
-
-	links_tmdbId = links.drop(['imdbId'],axis=1)
-	# top20MoviesArr = top20Movies(ratings,movies,links)
-	# print(top20MoviesArr)
-	# print(links_tmdbId)
+	genre_li = request.session['user_genre']
+	print('---------TOP Movies--------------------')
+	print(genre_li)
+	# links_tmdbId = links.drop(['imdbId'],axis=1)
 	
-	# return HttpResponse("<h1>Hello World top_Movies</h1>")
 	return render(request,"fetchmovie/top_movies.html",{})
 
 def dummy(request):
 
 	if 'id' in request.session:
 		del request.session['id']
-
+	if 'firstCall' in request.session:
+		del request.session['firstCall']
+	if 'user_genre' in request.session:
+		del request.session['user_genre']
 	
 	moviesVar = movies 
 
@@ -257,7 +383,7 @@ def dummy_old(request):
 	i = 0
 	for movie_name in action_movies_arr_final:
 		movies_links_tmdbId = movies_links_new.loc[movie_name,'tmdbId']
-		movie_response = getMovieDetail(movies_links_tmdbId)
+		movie_response = getMovieDetail1(movies_links_tmdbId)
 		# movie_response = 'Testing'
 		response_movie_detail.append(movie_response)
 		# print(movie_response)
@@ -268,7 +394,7 @@ def dummy_old(request):
 	return render(request,"fetchmovie/dummy.html", my_context)
 
 
-def getMovieDetail(movies_links_tmdbId):
+def getMovieDetail1(movies_links_tmdbId):
 	response = requests.get('https://api.themoviedb.org/3/movie/%s?api_key=5fd1f6ffb210aadcdec1444aaea3fc4a' %(movies_links_tmdbId))
 	response_movie_detail = response.json()
 	# print(response_movie_detail)
@@ -339,7 +465,7 @@ def getMoviesDetail(request):
 	response_movies_objects = []
 	i = 0
 	for tmdb in tmdbId:
-		movieObjects = getMovieDetail(tmdb)
+		movieObjects = getMovieDetail1(tmdb)
 		response_movies_objects.append(movieObjects)
 		# print(response_movies_objects)
 		i = i +1
